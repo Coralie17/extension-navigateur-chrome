f7ddafdc464609d3c6ea3c8e06374c207f7d9b45
Théo <theos9mar@gmail.com>
2024-01-09 15:55:39 +0100

    cinquième commit

--- actuscience.js
+++ actuscience.js
@@ -30,7 +30,7 @@ fetch(RSS_URL)
             const newDiv = document.createElement("div");
             const newContent = document.createTextNode(html);
             newDiv.appendChild(newContent);
-            const currentDiv = document.querySelector("FPdoLc 1J9FBc");
+            const currentDiv = document.querySelector("FPdoLc.1J9FBc");
             document.body.insertBefore(newDiv, currentDiv);
         }
 
