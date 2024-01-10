c9f722e4f803bbc22bba5049e51501af75ebb8ab
Théo <theos9mar@gmail.com>
2024-01-10 14:48:53 +0100

    septième commit

--- actuscience.js
+++ actuscience.js
@@ -2,6 +2,55 @@
 const RSS_URL = `https://lejournal.cnrs.fr/rss`;
 const RSS_URL2 = `https://www.pourlascience.fr/rss.xml`
 
+fetch(RSS_URL, RSS_URL2)
+    .then(response => response.text())
+    .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+    .then(data => {
+        console.log(data);
+
+// edit the html element that we insert into the html of google chrome home page.
+
+        const items = data.querySelectorAll("item");
+        let html = ``;
+        items.forEach(element => {
+            console.log("J'aime le chocolat")
+            html += `
+                <article>
+                    <img src="${element.querySelector("enclosure").innerHTML}/image/large.png" alt="">
+                    <h2>
+                        <a href="${element.querySelector("link").innerHTML}" target="_blank" rel="noopener">
+                        ${element.querySelector("title").innerHTML}
+                        </a>
+                    </h2>
+                </article>
+            `;
+        });
+
+// code that can be insert in the html home page
+
+         let addElement = () => {
+            console.log("il faut manger 5 fruit et légumes par jour.")
+             let newDiv = document.createElement("div");
+             newDiv.innerHTML = html
+             let currentDiv = document.querySelector(".o3j99.qarstb");
+             console.log(currentDiv, typeof currentDiv)
+
+             currentDiv.appendChild(newDiv)
+            
+            // if (currentDiv && currentDiv.parentNode) {
+            //     currentDiv.parentNode.insertBefore(newDiv, currentDiv);
+            // }
+            // else {
+            //     console.error("Error")
+            // }
+         }
+
+         document.onload = addElement();
+
+
+            // document.body.insertAdjacentHTML("afterend", html);
+     });
+
 fetch(RSS_URL)
     .then(response => response.text())
     .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
@@ -50,6 +99,7 @@ fetch(RSS_URL)
 
             // document.body.insertAdjacentHTML("afterend", html);
      });
+
 
 
 
